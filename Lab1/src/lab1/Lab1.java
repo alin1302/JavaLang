ab92cb065fc2e1122051596fb20f5b5c2a28a88a
alin1302 <mssalina343@gmail.com>
2017-11-06 21:00:16 +0300

    Done

---
+++ Lab1/src/lab1/Lab1.java
@@ -0,0 +1,15 @@
+
+package lab1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab1 {
+
+    public static void main(String[] args) {
+       int c = Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+       System.out.println(c);
+    }
+    
+}
